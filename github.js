a1e0f0f5007008ceaa8eb0827c67a9e8d4632dd1
keisuke-58 <69879340+keisuke-58@users.noreply.github.com>
2020-08-24 17:51:30 +0900

    Update github.js

--- github.js
+++ github.js
@@ -77,24 +77,23 @@ window.addEventListener('beforeinstallprompt', (event) => {
     deferredPrompt = event;
     // Update UI to notify the user they can add to home screen
     addBtn.disabled = false;
+    
+    addBtn.addEventListener('click', (e) => {
+        // hide our user interface that shows our A2HS button
+        console.log('click');
+        addBtn.disabled = true;
+        // Show the prompt
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice.then((choiceResult) => {
+            if(choiceResult.outcome === 'accepted'){
+              console.log('User accepted the A2HS prompt');
+            }else{
+              console.log('User dismissed the A2HS prompt');
+            }
+            deferredPrompt = null;
+        });
+    });
 });
-
-/*addBtn.addEventListener('click', (e) => {
-    // hide our user interface that shows our A2HS button
-    console.log('click');
-    addBtn.disabled = true;
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
-        } else {
-          console.log('User dismissed the A2HS prompt');
-        }
-        deferredPrompt = null;
-      });
-  });*/
-
 
 // ----------------------------------
