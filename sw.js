413c086a3aaf1a82714e2e2e1e4b779796726234
keisuke-58 <69879340+keisuke-58@users.noreply.github.com>
2020-08-24 15:19:13 +0900

    Update sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const cacheName = 'k-58-v1';
+const appShellFiles = [
+    '/android-chrome-192x192.png',
+    '/android-chrome-512x512.png',
+    '/apple-touch-icon.png',
+    '/browserconfig.xml',
+    '/favicon-16x16.png',
+    '/favicon-32x32.png',
+    '/favicon.ico',
+    '/github.js',
+    '/index.html',
+    '/mstile-144x144.png',
+    '/mstile-150x150.png',
+    '/mstile-310x150.png',
+    '/mstile-310x310.png',
+    '/mstile-70x70.png',
+    '/safari-pinned-tab.svg',
+    '/site.webmanifest',
+    '/style.css',
+    '/sw.js'
+];
