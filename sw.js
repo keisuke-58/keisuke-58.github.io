9759c4334b427977200b2fb520ad398f7324e4f8
keisuke-58 <69879340+keisuke-58@users.noreply.github.com>
2020-08-24 16:38:10 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,8 +6,23 @@ const urlsToCache = [
     '/style.css'
 ];
 
-self.addEventListener('fetch', (event) => {
-    //
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open(cacheName).then(function(cache) {
+     return cache.addAll([
+       '/index.html',
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
 });
 
 // install
