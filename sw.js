8b01ec25f9c937e4a8474948b7966f7581eb9804
keisuke-58 <69879340+keisuke-58@users.noreply.github.com>
2020-08-25 11:21:54 +0900

    a

--- sw.js
+++ sw.js
@@ -54,7 +54,7 @@ self.addEventListener('fetch', (e) => {
     );
 });
 
-// service worker update
+// update service worker
 self.addEventListener('activate', (e) => {
     let = cacheAllowlist = ['k-58-v2'];
     e.waitUntil(
@@ -71,56 +71,3 @@ self.addEventListener('activate', (e) => {
         })
     );
 });
-
-
-// install
-/*self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(cacheName).then((cache) => {
-            console.log('Opened cache');
-            return cache.addAll(urlsToCache);
-        })
-    );
-});*/
-
-// fetch cache load
-/*self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            //return response ? response : fetch(event.request);
-            if(response){
-                return response;   
-            }else{
-                let fetchRequest = event.request.clone();
-                return fetch(fetchRequest).then((response) => {
-                    if(!resopnse || response.status !== 200 || response.type !== 'basic'){
-                        return response;
-                    }
-                    
-                    let responseToCache = response.clone();
-                    caches.open(cacheName).then((cache) => {
-                        cache.put(event.request, resposeToCache);
-                    });
-                    
-                    return response;
-                });
-            }
-        })
-    );
-});*/
-
-// update service worker
-/*self.addEventListener('activate', (event) => {
-    let cacheAllowlist = ['pages-cache-v1'];
-    event.waitUntil(
-        caches.keys().then((cacheName) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if(cacheAllowlist.indexOf(cacheName) === -1){
-                        return caches.delete(cacheName);
-                    }
-                })
-            )
-        })
-    );
-});*/
