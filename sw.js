dd39acbbcdc5f261c6bbb749a312d6734d87894f
keisuke-58 <69879340+keisuke-58@users.noreply.github.com>
2020-08-25 17:06:25 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -51,13 +51,14 @@ self.addEventListener('activate', (e) => {
     let = cacheAllowlist = ['k-58-v2'];
     e.waitUntil(
         caches.keys().then((cacheName) => {
-            return Promise.all(
+            console.log(cacheName);
+            /*return Promise.all(
                 cacheNames.map((cacheName) => {
                     if(cacheAllowlist.indexOf(cacheName) !== -1){
                         return caches.delete(cacheName);
                     }
                 })
-            );
+            );*/
         })
     );
 });
