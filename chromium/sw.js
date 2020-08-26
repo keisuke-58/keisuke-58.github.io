f4d286692c62b94d8988eee13226e90dcae36818
keisuke-58 <69879340+keisuke-58@users.noreply.github.com>
2020-08-26 14:18:36 +0900

    Update sw.js

--- chromium/sw.js
+++ chromium/sw.js
@@ -1,25 +1,25 @@
 // install
-self.addEventListener('install', (e) => {
+self.addEventListener('install', function(e) {
     e.waitUntil(
-        caches.open('k-58-v1').then((cache) => {
+        caches.open('k-58-v1').then(function(cache) {
             return cache.addAll([
-                '/index.html',
-                '/github.js',
-                '/style.css',
-                '/ini.js',
-                '/android-chrome-192x192.png',
-                '/android-chrome-512x512.png'
-]           );
+                '/chromium/index.html',
+                '/chromium/github.js',
+                '/chromium/style.css',
+                '/chromium/ini.js',
+                '/chromium/android-chrome-192x192.png',
+                '/chromium/android-chrome-512x512.png'
+            ]);
         })
     );
 });
 
 // fetch cache load
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', function(e) {
     console.log(e.request.url);
     e.respondWith(
-        caches.match(e.request).then((response) => {
-            return response ? response : fetch(e.request);
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
         })
     );
 });
