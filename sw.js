65217f924ba40110022ad48cbcc56157438306da
keisuke-58 <69879340+keisuke-58@users.noreply.github.com>
2020-08-26 10:25:02 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,18 @@
-importScripts('/ini.js');
+//importScripts('/ini.js');
+const CACHE_NAME = 'k-58-v1';
+const SUCCESSIVE_CACHE = false;
+const URLS_TO_CACHE = [
+    '/index.html',
+    '/github.js',
+    '/style.css',
+    '/ini.js',
+    '/android-chrome-192x192.png',
+    '/android-chrome-512x512.png'
+];
+const CACHE_ALLOWLIST = [
+    'k-58-v2'
+];
+
 
 // install
 self.addEventListener('install', (e) => {
