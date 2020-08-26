8727d80e9abd22c9e1583a4e2c45ee4e12a7b9a0
keisuke-58 <69879340+keisuke-58@users.noreply.github.com>
2020-08-26 14:14:21 +0900

    Update sw.js

--- chromium/sw.js
+++ chromium/sw.js
@@ -19,50 +19,7 @@ self.addEventListener('fetch', (e) => {
     console.log(e.request.url);
     e.respondWith(
         caches.match(e.request).then((response) => {
-            //if(!SUCCESSIVE_CACHE){
-                return response ? response : fetch(e.request);
-            /*}else{
-                if(response){
-                    return response;   
-                }else{
-                    // リクエストを複製
-                    // リクエストはstreamのため１度しか使えないので複製
-                    // １回はキャッシュ、もう１回はフェッチのために使用
-                    let fetchRequest = e.request.clone();
-                    return fetch(fetchRequest).then((response) => {
-                        // 有効か否か
-                        // basicによりリクエストの送信元と送信先のドメインが同じであることを示す。サードパーティのアセットへのリクエストがキャッシュされないことと同義
-                        if(!response || response.status !== 200 || response.type !== 'basic'){
-                            return response;
-                        }
-                        
-                        // レスポンスの複製（stream）
-                        // キャッシュ、ブラウザ用
-                        let responseToCache = response.clone();
-                        caches.open(CACHE_NAME).then((cache) => {
-                            cache.put(e.request, responseToCache);
-                        });
-
-                        return response;
-                    })
-                }
-            }*/
+            return response ? response : fetch(e.request);
         })
     );
 });
-
-// update service worker
-/*self.addEventListener('activate', (e) => {
-    e.waitUntil(
-        caches.keys().then((cacheName) => {
-            console.log(cacheName);
-            /*return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if(CACHE_ALLOWLIST.indexOf(cacheName) !== -1){
-                        return caches.delete(cacheName);
-                    }
-                })
-            );*/
-        })
-    );
-});*/
