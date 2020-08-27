d7548fa020a4cbdd9d38d2585bbd29b2762d7910
keisuke-58 <69879340+keisuke-58@users.noreply.github.com>
2020-08-27 10:13:59 +0900

    a

--- digital-signage/github.js
+++ digital-signage/github.js
@@ -8,6 +8,31 @@ fetch('/digital-signage/ini.json').then((response) => {
     return response.json();
 }).then((json) => {
     console.log(json);
+    let html = '<div class="swiper-container"><div class="swiper-wrapper"><div class="swiper-slide">Slide 1</div><div class="swiper-slide">Slide 2</div><div class="swiper-slide">Slide 3</div></div><div class="swiper-pagination"></div><div class="swiper-button-prev"></div><div class="swiper-button-next"></div><div class="swiper-scrollbar"></div></div>';
+    
+    document.querySelector('#slider').insertAdjacentHTML('beforebegin', html).then(() => {
+        let mySwiper = new Swiper('.swiper-container', {
+            // Optional parameters
+            direction: 'vertical',
+            loop: true,
+
+            // If we need pagination
+            pagination: {
+                el: '.swiper-pagination',
+            },
+
+            // Navigation arrows
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+
+            // And if we need scrollbar
+            scrollbar: {
+                el: '.swiper-scrollbar',
+            },
+        });
+    });
 }).catch((error) => {
     console.log(error);
     alert('error : ' + error);
