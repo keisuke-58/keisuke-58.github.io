9a762af34d38d780b27865236a5a470e6e5b25ce
keisuke-58 <69879340+keisuke-58@users.noreply.github.com>
2020-08-27 12:04:24 +0900

    Update github.js

--- digital-signage/github.js
+++ digital-signage/github.js
@@ -8,7 +8,7 @@ fetch('/digital-signage/image.json').then((response) => {
     return response.json();
 }).then((json) => {
     console.log(json);
-    let html = '<div class="swiper-container" style="height:' + window.screen.height + 'px;"><div class="swiper-wrapper">';
+    let html = '<div class="swiper-container"><div class="swiper-wrapper"  style="height:' + window.screen.height + 'px;">';
     if(window.screen.width < window.screen.height){
         json['swiperOptions']['direction'] = 'vertical';
     }
