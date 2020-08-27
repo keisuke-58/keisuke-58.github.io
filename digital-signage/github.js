5923e957d452ebdbc5aa422491f262ddf0b8c55a
keisuke-58 <69879340+keisuke-58@users.noreply.github.com>
2020-08-27 10:16:56 +0900

    Update github.js

--- digital-signage/github.js
+++ digital-signage/github.js
@@ -10,28 +10,28 @@ fetch('/digital-signage/ini.json').then((response) => {
     console.log(json);
     let html = '<div class="swiper-container"><div class="swiper-wrapper"><div class="swiper-slide">Slide 1</div><div class="swiper-slide">Slide 2</div><div class="swiper-slide">Slide 3</div></div><div class="swiper-pagination"></div><div class="swiper-button-prev"></div><div class="swiper-button-next"></div><div class="swiper-scrollbar"></div></div>';
     
-    document.querySelector('#slider').insertAdjacentHTML('beforebegin', html).then(() => {
-        let mySwiper = new Swiper('.swiper-container', {
-            // Optional parameters
-            direction: 'vertical',
-            loop: true,
+    document.querySelector('#slider').insertAdjacentHTML('beforebegin', html);
+    
+    let mySwiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        direction: 'vertical',
+        loop: true,
 
-            // If we need pagination
-            pagination: {
-                el: '.swiper-pagination',
-            },
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
 
-            // Navigation arrows
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
 
-            // And if we need scrollbar
-            scrollbar: {
-                el: '.swiper-scrollbar',
-            },
-        });
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
     });
 }).catch((error) => {
     console.log(error);
