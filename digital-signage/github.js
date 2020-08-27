8f6727a0b914f498bfbd38cc2befe66bd6071db1
keisuke-58 <69879340+keisuke-58@users.noreply.github.com>
2020-08-27 10:31:31 +0900

    Update github.js

--- digital-signage/github.js
+++ digital-signage/github.js
@@ -8,30 +8,20 @@ fetch('/digital-signage/image.json').then((response) => {
     return response.json();
 }).then((json) => {
     console.log(json);
-    let html = '<div class="swiper-container"><div class="swiper-wrapper"><div class="swiper-slide">Slide 1</div><div class="swiper-slide">Slide 2</div><div class="swiper-slide">Slide 3</div></div><div class="swiper-pagination"></div><div class="swiper-button-prev"></div><div class="swiper-button-next"></div><div class="swiper-scrollbar"></div></div>';
+    let html = '<div class="swiper-container"><div class="swiper-wrapper"><div class="swiper-slide"><img src="degital-signage-sample.jpg"></div><div class="swiper-slide"><img src="degital-signage-sample.jpg"></div></div></div>';
     
     document.querySelector('#slider').insertAdjacentHTML('beforebegin', html);
     
     let mySwiper = new Swiper('.swiper-container', {
-        // Optional parameters
-        direction: 'vertical',
-        loop: true,
-
-        // If we need pagination
-        pagination: {
-            el: '.swiper-pagination',
-        },
-
-        // Navigation arrows
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-
-        // And if we need scrollbar
-        scrollbar: {
-            el: '.swiper-scrollbar',
-        },
+        direction : 'horizontal',
+        loop : true, 
+        speed : 10000,
+        autoHeight : true,
+        effect : 'slide', 
+        spaceBetween : 0,
+        slidePerView : 1,
+        slidePerGroup : 1,
+        centeredSlides : false
     });
 }).catch((error) => {
     console.log(error);
