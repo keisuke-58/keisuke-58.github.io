9102b0e06faa0a07f9873d53ea61a2ca280248d2
keisuke-58 <69879340+keisuke-58@users.noreply.github.com>
2020-08-31 14:36:05 +0900

    a

--- digital-signage/ini.js
+++ digital-signage/ini.js
@@ -8,7 +8,8 @@ const URLS_TO_CACHE = [
     '/digital-signage/android-chrome-192x192.png',
     '/digital-signage/android-chrome-512x512.png',
     '/digital-signage/digital-signage-sample.jpg',
-    '/digital-signage/digital-signage-sample-2.jpg'
+    '/digital-signage/digital-signage-sample-2.jpg',
+    '/digital-signage/digital-signage-sample-3.jpg'
 ];
 const CACHE_ALLOWLIST = [
     'k-58-digital-signage-v1'
