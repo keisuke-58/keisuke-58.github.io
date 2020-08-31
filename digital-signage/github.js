90be97459033b12c2413c69063db829d7447e54a
keisuke-58 <69879340+keisuke-58@users.noreply.github.com>
2020-08-31 10:51:22 +0900

    a

--- digital-signage/github.js
+++ digital-signage/github.js
@@ -27,26 +27,7 @@ fetch('/digital-signage/image.json').then((response) => {
     alert('error : ' + error);
 });
 
-// action navigator
-window.addEventListener('load', () => {
-    const ua = window.navigator.userAgent.toLowerCase();
-    console.log(ua);
-    if(ua.indexOf('windows nt') !== -1){
-        document.querySelector('body').insertAdjacentHTML('afterbegin', '<div class="message">「ALT + F4」で閉じる</div>');
-        fadeEvent();
-    }else if(ua.indexOf('mac os x') !== -1){
-        document.querySelector('body').insertAdjacentHTML('afterbegin', '<div class="message">「command + Q」で閉じる</div>');
-        fadeEvent();
-    }
-});
-function fadeEvent(){
-    document.querySelector('section').addEventListener('click', () => {
-        document.querySelector('.message').classList.toggle('fade');
-    });
-    document.querySelector('.message').addEventListener('click', () => {
-        document.querySelector('.message').classList.toggle('fade');
-    });
-}
+
 
 // ----------------------------------
 
