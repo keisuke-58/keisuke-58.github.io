6ef93b6c2303545ef82a2b8d4742613e6053cf78
keisuke-58 <69879340+keisuke-58@users.noreply.github.com>
2020-08-31 15:33:03 +0900

    a

--- pwa-cache/sw.js
+++ pwa-cache/sw.js
@@ -16,7 +16,7 @@ const CACHE_ALLOWLIST = [
 self.addEventListener('install', e => {
     console.log('V1 install');
     e.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.add('/digital-signage-sample.jpg'))
+        caches.open(CACHE_NAME).then(cache => cache.add('/pwa-cache/digital-signage-sample.jpg'))
     );
 });
 
@@ -26,7 +26,7 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
     const url = new URL(e.request.url);
-    if(url.origin == location.origin && url.pathname == '/digital-signage-sample-2.jpg'){
-        e.respondWith(caches.match('/digital-signage-sample.jpg'));
+    if(url.origin == location.origin && url.pathname == '/pwa-cache/digital-signage-sample-2.jpg'){
+        e.respondWith(caches.match('/pwa-cache/digital-signage-sample.jpg'));
     }
 });
