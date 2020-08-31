fcd1881d951aabf5a97b0996f58d0da167f6307d
keisuke-58 <69879340+keisuke-58@users.noreply.github.com>
2020-08-31 15:48:47 +0900

    a

--- pwa-cache/ini.js
+++ pwa-cache/ini.js
@@ -1,4 +1,4 @@
 const CACHE_NAME = 'pwa-cache-v1';
 const CACHE_ALLOWLIST = [
-    'k-58-v2'
+    'pwa-cache-v2'
 ];
--- pwa-cache/sw.js
+++ pwa-cache/sw.js
@@ -1,32 +1,51 @@
 importScripts('/pwa-examples/ini.js');
-/*const CACHE_NAME = 'k-58-v1';
-const SUCCESSIVE_CACHE = true;
-const URLS_TO_CACHE = [
-    '/pwa-examples/index.html',
-    '/pwa-examples/github.js',
-    '/pwa-examples/style.css',
-    '/pwa-examples/ini.js',
-    '/pwa-examples/android-chrome-192x192.png',
-    '/pwa-examples/android-chrome-512x512.png'
-];
+/*const CACHE_NAME = 'pwa-cache-v1';
 const CACHE_ALLOWLIST = [
-    'k-58-v2'
+    'pwa-cache-v2'
 ];*/
 
 self.addEventListener('install', e => {
-    console.log('V1 install');
+    // V1
+    /*console.log('V1 install');
     e.waitUntil(
         caches.open(CACHE_NAME).then(cache => cache.add('/pwa-cache/digital-signage-sample.jpg'))
+    );*/
+    
+    // V2
+    console.log('V2 install');
+    e.waitUntil(
+        caches.open('pwa-cache-v2').then(cache => cache.add('/pwa-cache/digital-signage-sample-2.jpg'));
     );
 });
 
 self.addEventListener('activate', e => {
-    console.log('V1 activate');
+    // V1
+    //console.log('V1 activate');
+    
+    // V2
+    e.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.map(key => {
+                if(!CACHE_ALLOWLIST.includes(key)){
+                    return caches.delete(key);
+                }
+            })
+        )).then(() => {
+            console.log('V2 activate');
+        });
+    );
 });
 
 self.addEventListener('fetch', e => {
+    // V1
+    /*const url = new URL(e.request.url);
+    if(url.origin == location.origin && url.pathname == '/pwa-cache/digital-signage-sample.jpg'){
+        e.respondWith(caches.match('/pwa-cache/digital-signage-sample-2.jpg'));
+    }*/
+    
+    // V2
     const url = new URL(e.request.url);
-    if(url.origin == location.origin && url.pathname == '/pwa-cache/digital-signage-sample-2.jpg'){
-        e.respondWith(caches.match('/pwa-cache/digital-signage-sample.jpg'));
+    if(url.origin == location.origin && url.pathname == '/pwa-cache/digital-signage-sample.jpg'){
+        e.respondWith(caches.match('/pwa-cache/digital-signage-sample-3.jpg'));
     }
 });
