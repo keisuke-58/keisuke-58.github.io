adb1b810aec0692afba632bc6456d655135bd54d
keisuke-58 <69879340+keisuke-58@users.noreply.github.com>
2020-08-31 10:59:29 +0900

    Update github.js

--- digital-signage/github.js
+++ digital-signage/github.js
@@ -60,13 +60,14 @@ window.addEventListener('beforeinstallprompt', (event) => {
         // hide our user interface that shows our A2HS button
         console.log('click');
         addBtn.style.display = 'none';
+        importScripts('/digital-signage/action.js');
         // Show the prompt
         deferredPrompt.prompt();
         // Wait for the user to respond to the prompt
         deferredPrompt.userChoice.then((choiceResult) => {
             if(choiceResult.outcome === 'accepted'){
                 console.log('User accepted the A2HS prompt');
-                importScripts('/digital-signage/action.js');
+                //importScripts('/digital-signage/action.js');
             }else{
                 console.log('User dismissed the A2HS prompt');
             }
