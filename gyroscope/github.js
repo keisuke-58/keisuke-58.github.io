5164262248a79563abc0cbc9fdd8e78fe7b96fc1
keisuke-58 <69879340+keisuke-58@users.noreply.github.com>
2020-09-03 12:04:30 +0900

    Update github.js

--- gyroscope/github.js
+++ gyroscope/github.js
@@ -3,14 +3,14 @@
 // ----------------------------------
 
 // main DOM
-let promise1 = new Promise(() => {
+/*let promise1 = new Promise(() => {
     document.querySelector('body').insertAdjacentHTML('afterbegin', '<button id="addButton" style="display:none;">Add to home screen</button><section id="gyro"><input type="txet" id="beforeGamma"><input type="txet" id="afterGamma"></section>');
 });
 
 let gyro = {}
 promise1.then(() => {
     window.addEventListener('deviceorientation', (event) => {
-        /*gyro['before'] = !gyro['after'] ? {} : gyro['after'];
+        gyro['before'] = !gyro['after'] ? {} : gyro['after'];
         gyro['after'] = {
             absolute : event.absolute,
             alpha : event.alpha,
@@ -18,10 +18,13 @@ promise1.then(() => {
             gamma : event.gamma
         }
         document.querySelector('#beforeGamma').value = gyro['before']['gamma'] === undefined ? '-' : gyro['before']['gamma'];
-        document.querySelector('#afterGamma').value = gyro['after']['gamma'];*/
-        alert(event.gamma);
+        document.querySelector('#afterGamma').value = gyro['after']['gamma'];
     }, true);
-});
+});*/
+
+window.addEventListener('devicemotion', (event) => {
+    alert(event.rotationRate.gamma);
+}, true);
 
 // ----------------------------------
 
