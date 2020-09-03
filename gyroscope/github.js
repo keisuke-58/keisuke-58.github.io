f21f883e72562fdaa2b06e50262e5790d840e7fc
keisuke-58 <69879340+keisuke-58@users.noreply.github.com>
2020-09-03 12:24:33 +0900

    Update github.js

--- gyroscope/github.js
+++ gyroscope/github.js
@@ -4,7 +4,7 @@
 
 // main DOM
 let promise1 = new Promise(() => {
-    document.querySelector('body').insertAdjacentHTML('afterbegin', '<button id="addButton" style="display:none;">Add to home screen</button><section id="gyro"><input type="txet" id="beforeGamma"><input type="txet" id="afterGamma"></section>');
+    document.querySelector('body').insertAdjacentHTML('afterbegin', '<button id="addButton" style="display:none;">Add to home screen</button><button id="start-button">start motion</button');
 });
 
 let gyro = {}
@@ -22,9 +22,36 @@ promise1.then(() => {
     }, true);*/
 });
 
-window.addEventListener('devicemotion', (event) => {
-    alert(event.rotationRate.gamma);
-}, true);
+
+const requestDeviceMotionPermission = () => {
+  if (
+    DeviceMotionEvent &&
+    typeof DeviceMotionEvent.requestPermission === 'function'
+  ) {
+    // iOS 13+ の Safari
+    // 許可を取得
+    DeviceMotionEvent.requestPermission()
+    .then(permissionState => {
+      if (permissionState === 'granted') {
+        // 許可を得られた場合、devicemotionをイベントリスナーに追加
+        window.addEventListener('devicemotion', e => {
+          // devicemotionのイベント処理
+            alert(e.rotationRate.gamma);
+        }）
+      } else {
+        // 許可を得られなかった場合の処理
+      }
+    })
+    .catch(console.error) // https通信でない場合などで許可を取得できなかった場合
+  } else {
+    // 上記以外のブラウザ
+  }
+}
+
+// ボタンクリックでrequestDeviceMotionPermission実行
+const startButton = document.getElementById("start-button")
+startButton.addEventListener('click', requestDeviceMotionPermission, false)
+
 
 // ----------------------------------
 
