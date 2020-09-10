53521c948d2b414f122a69818276f7316ab0ec37
keisuke-58 <69879340+keisuke-58@users.noreply.github.com>
2020-09-10 18:59:02 +0900

    Update loading.js

--- loading/loading.js
+++ loading/loading.js
@@ -2,7 +2,17 @@
 
 // ----------------------------------
 
-/*document.querySelector('#loadingButton').addEventListener('click', () => {
+let loadingType = fetch('select-loading.json').then((response) => {
+    if(!response || response.status !== 200 || response.type !== 'basic'){ throw 'loading.json error'; }
+    return response.json();
+}).then((json) => {
+    loadingType = json['type'];
+}).catch((error) => {
+    console.log(error);
+});
+
+document.querySelector('#loadingButton').addEventListener('click', () => {
+    console.log(loadingType);
     fetch('loading.json').then((response) => {
         if(!response || response.status !== 200 || response.type !== 'basic'){ throw 'loading.json error'; }
         return response.json();
@@ -17,7 +27,7 @@
         console.log(error);
     });
     
-});*/
+});
 
 function loading(boolean, json){
     const loading = document.querySelector('#' + json['id']);
@@ -29,36 +39,3 @@ function loading(boolean, json){
         }
     }
 }
-
-let loadingType;
-let promise1 = fetch('select-loading.json').then((response) => {
-    if(!response || response.status !== 200 || response.type !== 'basic'){ throw 'loading.json error'; }
-    return response.json();
-}).then((json) => {
-    loadingType = json['type'];
-}).catch((error) => {
-    console.log(error);
-});
-
-let promise2 = fetch('loading.json').then((response) => {
-    if(!response || response.status !== 200 || response.type !== 'basic'){ throw 'loading.json error'; }
-    return response.json();
-}).then((json) => {
-    console.log(json);
-    document.querySelector('body').insertAdjacentHTML('beforeend', '<div id="' + json['id'] + '"></div>');
-    return json;
-//}).then((json) => {
-    //loading(true, json);
-    //setTimeout(() => { loading(false, json); }, 5000);
-}).catch((error) => {
-    console.log(error);
-});
-
-document.querySelector('#loadingButton').addEventListener('click', () => {
-    promise1.then(promise2).then((json) => {
-        loading(true, json);
-        setTimeout(() => { loading(false, json); }, 5000);
-    }).catch((error) => {
-        console.log(error);
-    });
-});
