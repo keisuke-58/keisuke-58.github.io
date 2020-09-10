795e93a57136db5b99cc84995f2e6e07ec7bb764
keisuke-58 <69879340+keisuke-58@users.noreply.github.com>
2020-09-10 16:56:00 +0900

    a

--- loading/loading.js
+++ loading/loading.js
@@ -1,8 +1,29 @@
-fetch('loading.json').then((response) => {
-    if(!response || response.status !== 200 || response.type !== 'basic'){ throw 'ini.json error'; }
-    return response.json();
-}).then((json) => {
-    console.log(json);
-}).catch((error) => {
-    console.log(error);
+'use strict';
+
+// ----------------------------------
+
+document.querySelector('#loadingButton').addEventListener('click', () => {
+    fetch('loading.json').then((response) => {
+        if(!response || response.status !== 200 || response.type !== 'basic'){ throw 'ini.json error'; }
+        return response.json();
+    }).then((json) => {
+        console.log(json);
+        document.querySelector('body').insertAdjacentHTML('beforeend', '<div id="' + json['id'] + '"></div>');
+    }).then(() => {
+        loading(true, json);
+        setTimeout(() => { loading(false, json); }, 5000);
+    }).catch((error) => {
+        console.log(error);
+    });
 });
+
+function loading(boolean, json){
+    const loading = document.querySelector(json['id']);
+    if(boolean === true){
+        loading.insertAdjacentHTML('beforeend', json['type']['html']);
+        loading.style.display = 'block';
+    }else{
+        loading.style.display = 'none';
+        loading.textContent = null;
+    }
+}
