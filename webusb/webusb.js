af25bb5bc727a04ab171ae0a40b9c025de315a0c
keisuke-58 <69879340+keisuke-58@users.noreply.github.com>
2020-09-27 18:46:38 +0900

    Update webusb.js

--- webusb/webusb.js
+++ webusb/webusb.js
@@ -3,12 +3,9 @@
 //const ProductID = '0x06c3';
 
 document.addEventListener('DOMContentLoaded', async () => {
-  let devices = await navigator.usb.getDevices();
-  devices.forEach(device => {
-      navigator.usb.requestDevice({
-          filters : []
-      }).then((device) => {
-          console.log(device);
-      });
-  });
+    let devices = await navigator.usb.getDevices();
+    console.log(devices);
+    devices.forEach((device) => {
+      
+    });
 });
