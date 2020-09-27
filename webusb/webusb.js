efd5dfc01deaded3e461159ac6e80e348a3de032
keisuke-58 <69879340+keisuke-58@users.noreply.github.com>
2020-09-27 18:00:47 +0900

    Update webusb.js

--- webusb/webusb.js
+++ webusb/webusb.js
@@ -1,12 +1,18 @@
 
-const VendorID = '0x054c';
-const ProductID = '0x06c3';
+//const VendorID = '0x054c';
+//const ProductID = '0x06c3';
+
+let device;
 
 navigator.usb.requestDevice({
-    filters : []
-}).then((device) => {
-    console.log(device.productName);
-    console.log(device.manufacturerName);
+    filters : [
+        {
+            vendorId : 0x054c
+        }
+    ]
+}).then((selectedDevice) => {
+    console.log(selectedDevice.productName);
+    console.log(selectedDevice.manufacturerName);
 }).catch((error) => {
     console.log(error);
 });
