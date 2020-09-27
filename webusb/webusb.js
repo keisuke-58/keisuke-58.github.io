cd3e7168c7b9b6669ba89d9d7599a475055e2400
keisuke-58 <69879340+keisuke-58@users.noreply.github.com>
2020-09-27 17:39:49 +0900

    a

--- webusb/webusb.js
+++ webusb/webusb.js
@@ -1,15 +1,11 @@
-<script>
-    const VendorID = '0x054c';
-    const ProductID = '0x06c3';
-    const device = await navigator.usb.requestDevice({filters : [
-        {
-            vendorId : VendorID, 
-            product_id : ProductID
-        }
-    ]});
-    device.configurations;
+
+const VendorID = '0x054c';
+const ProductID = '0x06c3';
+const device = await navigator.usb.requestDevice({filters : [
+
+]});
+device.configurations;
         
-    //await device.open()
-    //await device.selectConfiguration(1)
-    //await device.claimInterface(0)
-</script>
+//await device.open()
+//await device.selectConfiguration(1)
+//await device.claimInterface(0)
