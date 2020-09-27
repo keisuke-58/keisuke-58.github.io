cd350892baf62a0af431d0dd719fb666aa6bd657
keisuke-58 <69879340+keisuke-58@users.noreply.github.com>
2020-09-27 19:15:48 +0900

    Update webusb.js

--- webusb/webusb.js
+++ webusb/webusb.js
@@ -6,6 +6,6 @@ document.addEventListener('DOMContentLoaded', async () => {
     let devices = await navigator.usb.getDevices();
     console.log(devices);
     devices.forEach((device) => {
-      
+        console.log("Product name: " + device.productName + ", serial number " + device.serialNumber);
     });
 });
