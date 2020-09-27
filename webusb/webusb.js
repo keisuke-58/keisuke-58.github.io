14e39bd92d818aec275e16c8ebd60e73c92c1e85
keisuke-58 <69879340+keisuke-58@users.noreply.github.com>
2020-09-27 22:09:08 +0900

    a

--- webusb/webusb.js
+++ webusb/webusb.js
@@ -2,10 +2,17 @@
 //const VendorID = '0x054c';
 //const ProductID = '0x06c3';
 
-document.addEventListener('DOMContentLoaded', async () => {
+/*document.addEventListener('DOMContentLoaded', async () => {
     let devices = await navigator.usb.getDevices();
     console.log(devices);
     devices.forEach((device) => {
+        console.log("Product name: " + device.productName + ", serial number " + device.serialNumber);
+    });
+});*/
+
+navigator.usb.getDevices().then((devices) => {
+    console.log("Total devices: " + devices.length);
+    devices.forEach(device => {
         console.log("Product name: " + device.productName + ", serial number " + device.serialNumber);
     });
 });
